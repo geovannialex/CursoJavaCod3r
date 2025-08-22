0a67a078369623fab84e84b608ef545dece406a3
GeovanniAlex <geovannialexandre2@gmail.com>
2025-08-21 23:53:54 -0300

    Aprendendo a utilizar continue

--- src/controle/Break.java
+++ src/controle/Break.java
@@ -6,7 +6,7 @@ public class Break {
 
         for (int i = 0; i <= 10; i++) {
 
-            if ( - ==5) {
+            if ( i == 5) {
                 break;
             }
             System.out.println(i);
